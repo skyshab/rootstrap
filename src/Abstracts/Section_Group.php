d2b6b8a188e0735990293c77ec1e93c50799b222
Sky Shabatura <sky@taprootstudios.com>
2019-03-13 15:30:29 -0500

    Added conditional to check for panel

--- src/Abstracts/Section_Group.php
+++ src/Abstracts/Section_Group.php
@@ -28,16 +28,16 @@ abstract class Section_Group {
      *
      * @since  1.0.0
      * @access protected
-     * @var    array 
+     * @var    array
      */
-    public $customize;    
+    public $customize;
 
     /**
      * Sections array.
      *
      * @since  1.0.0
      * @access private
-     * @var    array 
+     * @var    array
      */
     public $sections;
 
@@ -55,10 +55,10 @@ abstract class Section_Group {
      *
      * @since  1.0.0
      * @access private
-     * @var    bool 
+     * @var    bool
      */
     private $reverse;
-    
+
 
     /**
      * Register a new Sections Group object.
@@ -77,15 +77,15 @@ abstract class Section_Group {
         // set the object properties
         foreach( $args as $property => $value ) {
             if( property_exists( $this, $property ) )
-                $this->$property = $value;            
+                $this->$property = $value;
         }
-        
+
         // if reverse order is specified, flip the sections
         if( $this->reverse ) $this->sections = array_reverse( $this->sections );
 
         // add control for hiding section sections
         $this->section_hider();
-        
+
         // build the sections
         $this->section_loop();
     }
@@ -103,16 +103,16 @@ abstract class Section_Group {
         if( !$this->sections ) return;
 
         foreach( $this->sections as $id => $args ) {
-            
+
             // get the section
             $section = $this->customize->get_section( $id );
 
             if( !$section ) continue;
-    
-            // if hide flag enabled, set priority 
-            if( $this->is_section_hidden( $id ) ) 
-                $section->priority = 1000;                 
-                
+
+            // if hide flag enabled, set priority
+            if( $this->is_section_hidden( $id ) )
+                $section->priority = 1000;
+
             if( $this->title )
                 $section->title = $this->title;
 
@@ -120,7 +120,7 @@ abstract class Section_Group {
             $device = ( isset( $args['device'] ) ) ? $args['device'] : false;
 
             if( $device )
-                $section->type = sprintf( 'rootstrap-device--%s', $device );                   
+                $section->type = sprintf( 'rootstrap-device--%s', $device );
 
             // create setting and control
             $this->control( $id );
@@ -145,11 +145,15 @@ abstract class Section_Group {
      * @access public
      * @return string
      */
-    private function get_panel() {        
+    private function get_panel() {
         $section = $this->customize->get_section( key( $this->sections ) );
-        return $section->panel;
+
+        if ( $section && $section->panel ) {
+            return $section->panel;
+        }
+        return false;
     }
-    
+
 
     /**
      * Add the section hider
@@ -163,21 +167,21 @@ abstract class Section_Group {
         $this->customize->add_section( $id, [
             'priority' => 999,
             'panel' => $this->get_panel(),
-        ]); 
+        ]);
     }
 
-    
+
     /**
      * Is section hidden?
      *
      * @since  1.0.0
      * @access public
-     * @param  array    $args 
+     * @param  array    $args
      * @return bool
      */
     private function is_section_hidden( $id ) {
         $section = $this->sections[$id];
         return ( isset( $section['hide'] ) && $section['hide'] ) ? true : false;
-    }  
+    }
 
 }
