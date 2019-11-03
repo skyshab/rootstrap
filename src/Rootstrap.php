496d94e26d392eff2f1368c4dfb871414d5abbdc
Sky Shabatura <sky@sky.camp>
2019-11-03 10:53:59 -0600

    Condensed hooks.

--- src/Rootstrap.php
+++ src/Rootstrap.php
@@ -43,37 +43,13 @@ class Rootstrap implements Bootable {
         add_filter( 'rootstrap/vendor',   [$this, 'getVendorPath'] );
 
         // Load any classes and functions that will be needed in and out of the customizer.
-        add_action( 'after_setup_theme',  [$this, 'setup'],                 PHP_INT_MAX );
-
-        // Setup is complete. Configure any modules before implementation
-        add_action( 'after_setup_theme',  [$this, 'config'],                PHP_INT_MAX );
-
-        // Implement Rootstrap modules
-        add_action( 'after_setup_theme',  [$this, 'loaded'],                PHP_INT_MAX );
+        add_action( 'after_setup_theme',  [$this, 'setup'], PHP_INT_MAX );
 
         // Load any classes or functions needed only in the customize register hook
-        add_action( 'customize_register', [$this, 'customize_register'],    10 );
-
-        // Register custom controls, sections, panels
-        add_action( 'customize_register', [$this, 'custom'],                20 );
-
-        // Register Panels
-        add_action( 'customize_register', [$this, 'panels'],                30 );
-
-        // Register Sections
-        add_action( 'customize_register', [$this, 'sections'],              40 );
-
-        // Register Settings
-        add_action( 'customize_register', [$this, 'settings'],              50 );
-
-        // Register Controls
-        add_action( 'customize_register', [$this, 'controls'],              60 );
-
-        // Register Controls
-        add_action( 'customize_register', [$this, 'partials'],              70 );
+        add_action( 'customize_register', [$this, 'customize_register'] );
 
         // Modify registered components
-        add_action( 'customize_register', [$this, 'after'],                 PHP_INT_MAX );
+        add_action( 'customize_register', [$this, 'after'], PHP_INT_MAX );
     }
 
     /**
@@ -88,28 +64,6 @@ class Rootstrap implements Bootable {
     }
 
     /**
-     * Configure any Rootstrap modules prior to implementation.
-     *
-     * @since  1.0.0
-     * @access public
-     * @return void
-     */
-    public function config() {
-        do_action( 'rootstrap/config' );
-    }
-
-    /**
-     * Implement modules.
-     *
-     * @since  1.0.0
-     * @access public
-     * @return void
-     */
-    public function loaded() {
-        do_action( 'rootstrap/loaded' );
-    }
-
-    /**
      * Load any classes or functions needed only in the customize register hook.
      *
      * @since  1.0.0
@@ -117,73 +71,18 @@ class Rootstrap implements Bootable {
      * @return void
      */
     public function customize_register($manager) {
-        do_action('rootstrap/customize-register', $manager);
-    }
 
-    /**
-     * Register custom controls, sections, panels.
-     *
-     * @since  1.0.0
-     * @access public
-     * @return void
-     */
-    public function custom($manager) {
-        do_action('rootstrap/customize-register/custom', $manager);
-    }
-
-    /**
-     * Register panels.
-     *
-     * @since  1.0.0
-     * @access public
-     * @return void
-     */
-    public function panels($manager) {
-        do_action('rootstrap/customize-register/panels', $manager);
-    }
-
-    /**
-     * Register sections.
-     *
-     * @since  1.0.0
-     * @access public
-     * @return void
-     */
-    public function sections($manager) {
-        do_action('rootstrap/customize-register/sections', $manager);
-    }
-
-    /**
-     * Register settings.
-     *
-     * @since  1.0.0
-     * @access public
-     * @return void
-     */
-    public function settings($manager) {
-        do_action('rootstrap/customize-register/settings', $manager);
-    }
-
-    /**
-     * Register controls.
-     *
-     * @since  1.0.0
-     * @access public
-     * @return void
-     */
-    public function controls($manager) {
-        do_action('rootstrap/customize-register/controls', $manager);
-    }
-
-    /**
-     * Register partials.
-     *
-     * @since  1.0.0
-     * @access public
-     * @return void
-     */
-    public function partials($manager) {
-        do_action('rootstrap/customize-register/partials', $manager);
+        // Customize register actions
+        array_map(function($hook) use ($manager) {
+            do_action( "rootstrap/{$hook}", $manager);
+        }, [
+            'customize-register',
+            'customize-register/panels',
+            'customize-register/sections',
+            'customize-register/settings',
+            'customize-register/controls',
+            'customize-register/partials',
+        ]);
     }
 
     /**
@@ -205,7 +104,7 @@ class Rootstrap implements Bootable {
      * @return void
      */
     public function setVendorPath($path) {
-        if($path) {
+        if ($path) {
             $this->vendor_path = $path;
         }
     }
